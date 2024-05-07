d2b791a7cd890d2f9f76ec8ca95f6fd2f898f06f
vai_meraki <vaishnavi.jagtap4498@gmail.com>
2024-05-07 08:03:17 +0200

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,13 +1,15 @@
 --Query2
-UPDATE WideWorldImporters.Sales.InvoiceLines
-SET WideWorldImporters.Sales.InvoiceLines.UnitPrice 
-= WideWorldImporters.Sales.InvoiceLines.UnitPrice + 20
-WHERE WideWorldImporters.Sales.InvoiceLines.InvoiceLineID = 
-(SELECT TOP(1) il.InvoiceLineID
+UPDATE il
+SET il.UnitPrice = il.UnitPrice + 20
 FROM WideWorldImporters.Sales.InvoiceLines AS il
-JOIN WideWorldImporters.Sales.Invoices AS i 
-ON il.InvoiceID = i.InvoiceID
-JOIN WideWorldImporters.Sales.Orders AS o 
-ON i.OrderID = o.OrderID
+JOIN WideWorldImporters.Sales.Invoices AS i ON il.InvoiceID = i.InvoiceID
+JOIN WideWorldImporters.Sales.Orders AS o ON i.OrderID = o.OrderID
 WHERE o.CustomerID = 1060
-ORDER BY il.InvoiceLineID ASC)
+AND il.InvoiceLineID = (
+    SELECT TOP(1) il2.InvoiceLineID
+    FROM WideWorldImporters.Sales.InvoiceLines AS il2
+    JOIN WideWorldImporters.Sales.Invoices AS i2 ON il2.InvoiceID = i2.InvoiceID
+    JOIN WideWorldImporters.Sales.Orders AS o2 ON i2.OrderID = o2.OrderID
+    WHERE o2.CustomerID = 1060
+    ORDER BY il2.InvoiceLineID ASC
+);
