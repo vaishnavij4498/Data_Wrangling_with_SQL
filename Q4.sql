365513040e328cd89f84c3adfac77ab319f8eb0b
vai_meraki <vaishnavi.jagtap4498@gmail.com>
2024-05-07 08:06:28 +0200

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,34 +1,37 @@
 --Query 4
-WITH OrderLoss AS
-(SELECT c.CustomerName, c.CustomerID, 
-cc.CustomerCategoryName, SUM(ol.Quantity*ol.UnitPrice) 
-AS TotalLost
-FROM WideWorldImporters.Sales.CustomerCategories AS cc 
-JOIN WideWorldImporters.Sales.Customers AS c  
-ON cc.CustomerCategoryID = c.CustomerCategoryID
-JOIN WideWorldImporters.Sales.Orders AS o
-ON c.CustomerID = o.CustomerID
-JOIN WideWorldImporters.Sales.OrderLines AS ol 
-ON ol.OrderID = o.OrderID 
-WHERE NOT EXISTS
-(
-SELECT i.CustomerID
-FROM WideWorldImporters.Sales.Invoices AS i
-WHERE i.OrderID = o.OrderID
-)
-GROUP BY  c.CustomerName, 
-c.CustomerID, 
-cc.CustomerCategoryName
-), MaxLossInsideCategory AS 
-(SELECT ols.CustomerCategoryName, MAX(TotalLost) AS MaxLoss
-FROM OrderLoss As ols
-GROUP BY ols.CustomerCategoryName)
+SELECT ml.CustomerCategoryName,
+       ml.MaxLoss,
+       olss.CustomerName,
+       olss.CustomerID
+FROM (
+    SELECT cc.CustomerCategoryName,
+           MAX(ol.Quantity * ol.UnitPrice) AS MaxLoss
+    FROM WideWorldImporters.Sales.Orders AS o
+    JOIN WideWorldImporters.Sales.OrderLines AS ol ON o.OrderID = ol.OrderID
+    JOIN WideWorldImporters.Sales.Customers AS c ON o.CustomerID = c.CustomerID
+    JOIN WideWorldImporters.Sales.CustomerCategories AS cc ON c.CustomerCategoryID = cc.CustomerCategoryID
+    WHERE NOT EXISTS (
+            SELECT 1
+            FROM WideWorldImporters.Sales.Invoices AS i
+            WHERE i.OrderID = o.OrderID
+        )
+    GROUP BY cc.CustomerCategoryName
+) AS ml
+JOIN (
+    SELECT c.CustomerName,
+           c.CustomerID,
+           cc.CustomerCategoryName,
+           SUM(ol.Quantity * ol.UnitPrice) AS TotalLost
+    FROM WideWorldImporters.Sales.Orders AS o
+    JOIN WideWorldImporters.Sales.OrderLines AS ol ON o.OrderID = ol.OrderID
+    JOIN WideWorldImporters.Sales.Customers AS c ON o.CustomerID = c.CustomerID
+    JOIN WideWorldImporters.Sales.CustomerCategories AS cc ON c.CustomerCategoryID = cc.CustomerCategoryID
+    WHERE NOT EXISTS (
+            SELECT 1
+            FROM WideWorldImporters.Sales.Invoices AS i
+            WHERE i.OrderID = o.OrderID
+        )
+    GROUP BY c.CustomerName, c.CustomerID, cc.CustomerCategoryName
+) AS olss ON ml.CustomerCategoryName = olss.CustomerCategoryName AND ml.MaxLoss = olss.TotalLost
+ORDER BY ml.MaxLoss DESC;
 
-SELECT ml.CustomerCategoryName, ml.MaxLoss, 
-olss.CustomerName, olss.CustomerID
-FROM MaxLossInsideCategory AS ml 
-JOIN OrderLoss AS olss ON ml.MaxLoss = olss.TotalLost 
-AND ml.CustomerCategoryName = olss.CustomerCategoryName
-GROUP BY ml.CustomerCategoryName, ml.MaxLoss,
-olss.CustomerName, olss.CustomerID
-ORDER BY MaxLoss DESC
